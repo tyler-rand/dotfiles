1b39c88f6181726d2976b723e083660c37970ac1
Tyler Rand <tyler@tyler-rand.com>
2017-01-23 00:04:30 -0500

    initial commit - started from thoughtbot's dotfiles

---
+++ zsh/configs/post/path.zsh
@@ -0,0 +1,15 @@
+# ensure dotfiles bin directory is loaded first
+PATH="$HOME/.bin:/usr/local/sbin:$PATH"
+
+# load rbenv if available
+if command -v rbenv >/dev/null; then
+  eval "$(rbenv init - --no-rehash)"
+fi
+
+# mkdir .git/safe in the root of repositories you trust
+PATH=".git/safe/../../bin:$PATH"
+
+# load elixir
+PATH="/usr/local/bin/elixir/$PATH"
+
+export -U PATH
