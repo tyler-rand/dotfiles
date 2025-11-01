e2565fabc71083483956b8edc054eaa2d682610b
Tyler Rand <tyler@tyler-rand.com>
2025-11-01 12:02:51 -0400

    zsh: add asdf to path

--- zsh/configs/post/path.zsh
+++ zsh/configs/post/path.zsh
@@ -6,6 +6,9 @@ if command -v rbenv >/dev/null; then
   eval "$(rbenv init - --no-rehash)"
 fi
 
+# asdf for tool version management
+PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+
 # mkdir .git/safe in the root of repositories you trust
 PATH=".git/safe/../../bin:$PATH"
 
